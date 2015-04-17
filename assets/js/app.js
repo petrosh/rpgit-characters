1c6f03368dda5e525e3110278859c4c0a9648bf1
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-17 20:08:24 +0200

    responseText?

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,7 +13,7 @@ function init() {
 }
 
 function renderName() {
-  var resp = JSON.parse(this.data);
+  var resp = this.responseText;
   console.log(resp);
   // if(resp.content != ''){
   //   var title = document.getElementById('pageTitle').innerHTML = "Ciao " + resp.content;
