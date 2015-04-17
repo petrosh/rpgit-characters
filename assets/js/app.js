260d2ff60f43af31a775ed9c64305e3ded8ad424
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-17 20:32:50 +0200

    present my buddy tim

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,23 @@
 var pathArray = window.location.host.split( '.' );
 var pathSlash = window.location.pathname.split( '/' ); // pathSlash[1]
 var path = { 'username': pathArray[0], 'reponame': 'rpgit-characters' };
+var thi = document.getElementById("thi").innerHTML;
+var tname = document.getElementById("tname").innerHTML;
 
 init();
 
 function init() {
   // check if name (read character/name.log content)
   // /repos/:owner/:repo/contents/:path
-  var link = document.getElementById('editName');
-  link.href = "https://github.com/" + path['username'] + "/rpgit-characters/edit/gh-pages/character/name.log";
   getAPI( "repos/" + path['username'] + "/" + path['reponame'] + "/contents/character/name.log", renderName, 'raw' );
 }
 
 function renderName() {
   var resp = this.responseText;
   if(resp != ''){
-    var title = document.getElementById('pageTitle').innerHTML = "Ciao " + resp;
+    var ele = tim(thi, resp);
+  }else{
+    var ele = tim(tname, path);
   }
+  document.getElementsByTagName("section")[0].innerHTML = ele;
 }
