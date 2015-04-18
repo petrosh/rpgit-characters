db529e7025f24f0ce1232d94266ba4ee79181d08
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-18 19:57:05 +0200

    0-9 profiles

--- assets/js/app.js
+++ assets/js/app.js
@@ -73,12 +73,12 @@ function getName() {
 function getProfiles(  ){
   Math.seedrandom( arguments[0] );
   if ( arguments[1] ) {
-    for (var p = 1; p <= 10; p++) {
+    for (var p = 0; p < 10; p++) {
       char[p] = { st: die(2), de: die(2), in: die(2), en: die(2), ed: die(2), ss: die(2) };
     }
     return char[arguments[1]];
   }else{
-    for (var p = 1; p <= 10; p++) {
+    for (var p = 0; p < 10; p++) {
       char[p] = { st: die(2,1), de: die(2,1), in: die(2,1), en: die(2,1), ed: die(2,1), ss: die(2,1) };
     }
     return char;
