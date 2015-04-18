0843c0633ec0ed6b5fabcec4ac60155c108859e0
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-18 23:09:28 +0200

    tchance?

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@ var path = { username: pathArray[0], reponame: 'rpgit-characters' };
 
 var thi = Handlebars.compile( document.getElementById("thi").innerHTML );
 var tname = Handlebars.compile(document.getElementById("tname").innerHTML);
+var tchances = Handlebars.compile(document.getElementById("tchances").innerHTML);
+
 var char = [], characterName = '', profile = '', service = '';
 
 init();
@@ -76,13 +78,13 @@ function callbackChances() {
             }
           }
           partial[throws] = val;
-          console.log( service, throws, val);
         }
       }
       out[service] = partial;
     }
   }
-  console.log(out);
+  var ele = tchances( out );
+  document.getElementsByTagName("section")[0].innerHTML = ele;
 }
 
 function getName() {
