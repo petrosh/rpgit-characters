1a598f7297e6f9a8a68d48d0612a4edeb0ef5fd0
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-18 21:51:42 +0200

    val must be parsed

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,7 +63,7 @@ function callbackChances() {
             if(thro.hasOwnProperty(tt)){ // tt = 2d6, +1, ...
               var key = thro[tt]; // key = 10+, ss9+, ...
               if(tt=="2d6"){
-                val = key.substring(0,key.length-1);
+                val = parseInt( key.substring(0,key.length-1) );
               }else{
                 var att = upp[key.substring(0,2)];
                 console.log(key.substring(2,key.length-1));
