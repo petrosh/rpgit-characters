e82e6dfe71461619191afdea5dc6a687282bb7e5
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-18 17:36:30 +0200

    thi(char)

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,9 +62,9 @@ function getName() {
       char[p] = { st: die(2,1), de: die(2,1), in: die(2,1), en: die(2,1), ed: die(2,1), ss: die(2,1) };
     }
     console.log( char );
-    var ele = tim(thi, char );
+    var ele = thi( char );
   }else{
-    var ele = tim(tname, path);
+    var ele = tname( path );
   }
   document.getElementsByTagName("section")[0].innerHTML = ele;
 }
