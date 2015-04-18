c5846762968f91371ca8a19e6788d31ff3cca7a2
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-18 23:16:58 +0200

    service?

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ var path = { username: pathArray[0], reponame: 'rpgit-characters' };
 var thi = Handlebars.compile( document.getElementById("thi").innerHTML );
 var tname = Handlebars.compile(document.getElementById("tname").innerHTML);
 var tchances = Handlebars.compile(document.getElementById("tchances").innerHTML);
-var tservice = Handlebars.registerPartial("service", document.getElementById("tservices").innerHTML);
+var tservice = Handlebars.registerPartial("services", document.getElementById("tservices").innerHTML);
 
 var char = [], characterName = '', profile = '', service = '';
 
