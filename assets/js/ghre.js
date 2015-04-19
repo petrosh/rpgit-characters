0b8116ef35c15b738aea6c1ac107e1aeceb510d9
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-19 17:51:18 +0200

    no header sent nor received

--- assets/js/ghre.js
+++ assets/js/ghre.js
@@ -5,13 +5,12 @@ function getAPI(url, callback, media) {
     url,
     true
   );
-  xhr.setRequestHeader('Accept', 'application/vnd.github.v3.'+media+'+json');
+  // xhr.setRequestHeader('Accept', 'application/vnd.github.v3.'+media+'+json');
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
       // defensive check
       if (typeof callback == "function") {
         // apply() sets the meaning of "this" in the callback
-        document.getElementById("xremaining").innerHTML = 'remaining ' + xhr.getResponseHeader("X-RateLimit-Remaining");
         callback.apply(xhr);
       }
     }
