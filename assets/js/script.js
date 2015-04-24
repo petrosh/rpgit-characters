4c44ba732dec37a605e2c208eeecd3e0ee3619b5
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-24 19:12:05 +0200

    calm down

--- assets/js/script.js
+++ assets/js/script.js
@@ -106,7 +106,7 @@ function callbackVersion() {
   // System version retrive and save then check character name
   var resp = this.responseText;
   var cosa = JSON.parse(resp);
-  console.log({sha:lastVersionSha});
+  console.log({sha:cosa[0].commit.sha});
   lastVersionSha = cosa[0].commit.sha;
   // get character name
   getAPI( "https://cdn.rawgit.com/" + path.username + "/" + path.reponame + "/v0.1/character/name.txt", callbackName, fallbackName );
