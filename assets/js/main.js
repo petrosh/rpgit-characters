6df3da4497734813d9f4bf3ee4da58009bb9ae9a
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-27 16:13:59 +0200

    try xhr getFile

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,18 @@ Character.prototype.upp = function () {
 
 Character.prototype.name = function () {
   var url = "https://cdn.rawgit.com/" + this.username + "/" + this.reponame + "/v0.1/character/name.txt";
-  return url;
+  var name = getFile(url);
+  return name;
 };
+
+function getFile(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.open ("GET", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) return xhr.responseText;
+    if (xhr.readyState == 4 && xhr.status == 404) return url + " not found";
+  };
+  xhr.send();
+}
 
 var player = new Character();
