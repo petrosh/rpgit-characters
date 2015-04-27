16780bef525b859e218184228b62d026c9f4df50
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-27 17:24:10 +0200

    try

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,16 +8,20 @@ function Character () {
 
 }
 
-Character.prototype.setup = function () {
-  function setName (response) {
-    this.name = response;
+Character.prototype = {
+  name : function () {
+    return "https://cdn.rawgit.com/" + this.username + "/" + this.reponame + "/v0.1/character/name.txt";
   }
+};
+
+Character.prototype.setup = function () {
+
   var url = "https://cdn.rawgit.com/" + this.username + "/" + this.reponame + "/v0.1/character/name.txt";
   var xhr = new XMLHttpRequest();
   xhr.open ("GET", url, true);
   xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) setName(xhr.responseText);
-    if (xhr.readyState == 4 && xhr.status == 404) setName(url + " not found");
+    if (xhr.readyState == 4 && xhr.status == 200) return xhr.responseText;
+    if (xhr.readyState == 4 && xhr.status == 404) return url + " not found";
   };
   xhr.send();
 };
