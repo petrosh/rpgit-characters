ae430d5e9ebd940545472542740d258f7aaeb32a
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-27 16:26:46 +0200

    function on function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,16 @@ function Character () {
   this.username         = window.location.host.split('.')[0];
   this.reponame         = window.location.pathname.split('/')[1];
   this.seed             = this.username + this.reponame;
-  this.name             = getName(this);
-
+  this.name             = function () {
+    var url = "https://cdn.rawgit.com/" + obj.username + "/" + obj.reponame + "/v0.1/character/name.txt";
+    var xhr = new XMLHttpRequest();
+    xhr.open ("GET", url, true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) return xhr.responseText;
+      if (xhr.readyState == 4 && xhr.status == 404) return url + " not found";
+    };
+    xhr.send();
+  };
 }
 
 Character.prototype.upp = function () {
