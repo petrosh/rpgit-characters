b72f41ab0252ac98aa78cdef36488efdfde85974
Alex Petrosh <petrosh@Mac-Pro-di-petrosh.local>
2015-04-27 16:21:04 +0200

    getName

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@ function Character () {
   this.username         = window.location.host.split('.')[0];
   this.reponame         = window.location.pathname.split('/')[1];
   this.seed             = this.username + this.reponame;
+  this.name             = getName(this);
 
 }
 
@@ -10,8 +11,8 @@ Character.prototype.upp = function () {
   return this.name();
 };
 
-Character.prototype.name = function () {
-  var url = "https://cdn.rawgit.com/" + this.username + "/" + this.reponame + "/v0.1/character/name.txt";
+function getName (obj) {
+  var url = "https://cdn.rawgit.com/" + obj.username + "/" + obj.reponame + "/v0.1/character/name.txt";
   var xhr = new XMLHttpRequest();
   xhr.open ("GET", url, true);
   xhr.onreadystatechange = function() {
